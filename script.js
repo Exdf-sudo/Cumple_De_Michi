55f7ce0eb633a27963b52f76e019778d11015f37
Exdf-sudo <83036681+Exdf-sudo@users.noreply.github.com>
2026-06-10 13:15:24 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function irAsi() {
 function irAno() {
     window.location.href = "decline.html";
 }
+
+function irAweno() {
+    window.location.href = "weno.html";
+}
+
+function dispararAlerta() {
+    alert("Descarga en curso(7KB)...");
+}
